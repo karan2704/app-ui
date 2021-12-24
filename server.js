62e0b558415e85aff766d62ec73c5665b701a06c
Karan Mishra <karan.m2704@gmail.com>
2021-12-25 03:09:24 +0530

    mock server integration

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 const PORT = 8080
 
-app.get('/', (req, res) => {
+app.post('/api/state/cache', (req, res) => {
     res.status(204).json({
         error: false,
-        message: `Response from home route /`
+        message: "Message from server !!"
     })
 })
 
